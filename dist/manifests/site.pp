e6934caa83657d7b7185b171159e11cd441d3e3e
Birger Schmidt <bs-github@netgaroo.com>
2013-09-26 20:39:21 +0200

    dirty hack: accept unsigned packages for now

--- dist/manifests/site.pp
+++ dist/manifests/site.pp
@@ -8,6 +8,17 @@ node default {
   #
   Apt::Source <| |> -> Package <| |>
 
+  # FIXME: this is a dirty hack and should go away as soon as possible.
+  # The fix would be to sign the flapjack package propperly.
+  # For now, it's OK to install unsigned packages.
+  File ['/etc/apt/apt.conf.d/99auth']-> Package <| |>
+  file { '/etc/apt/apt.conf.d/99auth':
+    owner     => root,
+    group     => root,
+    content   => 'APT::Get::AllowUnauthenticated yes;',
+    mode      => '0644',
+  }
+
   class {'utils':} ->
   class {'stdlib': } ->
   class {'ruby::common': } ->
