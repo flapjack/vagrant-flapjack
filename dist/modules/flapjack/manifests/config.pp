cae775a6cc027447a29fdb7090fe770f0f909033
Birger Schmidt <bs-github@netgaroo.com>
2013-10-21 22:14:29 +0200

    configure and start flapjack via puppet

--- dist/modules/flapjack/manifests/config.pp
+++ dist/modules/flapjack/manifests/config.pp
@@ -1,5 +1,53 @@
 class flapjack::config {
+  file { '/var/run/flapjack':
+    ensure  => directory,
+    mode    => '0777',
+    require => [ Package['flapjack'] ],
+  }
+
+  file { '/var/log/flapjack':
+    ensure  => directory,
+    mode    => '0777',
+    require => [ Package['flapjack'] ],
+  }
+
+  file { '/etc/flapjack':
+    ensure  => directory,
+    require => [ Package['flapjack'] ],
+  }
+
   file { '/etc/flapjack/flapjack-config.yaml':
     source  => 'puppet:///modules/flapjack/etc/flapjack/flapjack-config.yaml',
   }
+
+  file { '/etc/init.d/flapjack-web-api':
+    source  => 'puppet:///modules/flapjack/etc/init.d/flapjack-web-api',
+  }
+
+  logrotate::rule { 'flapjack-log':
+    path          => '/var/log/flapjack/*.log',
+    maxage        => 31,
+    size          => '100M',
+    dateext       => true,
+    dateformat    => '.%Y%m%d',
+    copy          => true,
+    copytruncate  => true,
+    compress      => true,
+    compresscmd   => '/bin/bzip2',
+    compressext   => '.bz2',
+  }
+
+  logrotate::rule { 'flapjack-output':
+    path          => '/var/log/flapjack/*.output',
+    maxage        => 31,
+    size          => '100M',
+    dateext       => true,
+    dateformat    => '.%Y%m%d',
+    copy          => true,
+    copytruncate  => true,
+    compress      => true,
+    compresscmd   => '/bin/bzip2',
+    compressext   => '.bz2',
+  }
+
 }
--- dist/modules/flapjack/manifests/service.pp
+++ dist/modules/flapjack/manifests/service.pp
@@ -1,2 +1,17 @@
 class flapjack::service {
+
+  service { 'flapjack':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => false,
+    require    => [
+      File['/etc/flapjack/flapjack-config.yaml'],
+    ],
+    subscribe  => [
+      Package['flapjack'],
+      File['/etc/flapjack/flapjack-config.yaml'],
+    ]
+  }
+
 }
