aa332ae6e0a2391dc1efcfa355ec6fcd3c41a1b9
Jesse Reynolds <jesse@va.com.au>
2013-11-20 00:57:25 +1100

    #4 - clean up puppet manifests

--- dist/modules/flapjack/manifests/init.pp
+++ dist/modules/flapjack/manifests/init.pp
@@ -2,8 +2,8 @@ class flapjack {
 
   class{'flapjack::apt': } ->
   class{'flapjack::install': } ->
-  class{'flapjack::config': } ~>
-  class{'flapjack::service': } ->
+  #class{'flapjack::config': } ~>
+  #class{'flapjack::service': } ->
   class{'flapjack::flapjackfeeder': } ->
   Class['flapjack']
 }
--- dist/modules/ruby/files/rbenv.sh
+++
@@ -1,4 +0,0 @@
-# Add rbenv tools to path
-export PATH="/opt/rbenv/bin:$PATH"
-export RBENV_ROOT="/opt/rbenv"
-eval "$(rbenv init -)"
