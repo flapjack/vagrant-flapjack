ddb785ef6c78d8637dcff8eef9a35dfecd930803
Sarah Kowalik <sarah.kowalik@bulletproof.net>
2014-08-18 20:21:34 +1000

    Add serverspec to test if the flapjack/icinga/nagios processes are working

---
+++ spec/flapjack/flapjack_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe package('flapjack') do
+  it { should be_installed }
+end
+
+describe service('flapjack') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+describe process("redis-server") do
+  it { should be_running }
+  its(:args) { should match /127.0.0.1:6380/ }
+end
+
+describe process("flapjack") do
+  it { should be_running }
+  its(:args) { should match /\/opt\/flapjack\/bin\/flapjack start/ }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+describe port(3071) do
+  it { should be_listening }
+end
+describe port(3080) do
+  it { should be_listening }
+end
+describe port(3081) do
+  it { should be_listening }
+end
+describe port(6380) do
+  it { should be_listening }
+end
+
+describe file('/etc/flapjack/flapjack_config.yaml') do
+  it { should be_file }
+  its(:content) { should match /pagerduty/ }
+end
+
+describe file('/usr/local/lib/flapjackfeeder.o') do
+  it { should be_file }
+end
+
+describe file('/var/log/flapjack/flapjack.log') do
+  it { should be_file }
+end
+
+describe file('/var/log/flapjack/notification.log') do
+  it { should be_file }
+end
