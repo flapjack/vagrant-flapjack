643aa49095619207444fa7e479b27bfe5e012d51
Jesse Reynolds <jesse@va.com.au>
2014-09-13 15:16:47 -0400

    add ruby 1.9.3, flapjack-diner and pry ruby gems

--- dist/manifests/site.pp
+++ dist/manifests/site.pp
@@ -17,6 +17,25 @@ node default {
 
   class {'icinga': } ->
   class {'nagios': } ->
-  class {'flapjack': }
+  class {'flapjack': } ->
+
+  # FIXME: this is unlikely to work for trusty
+  package { 'ruby1.9.1-full':
+    ensure => present
+  } ->
+
+  package { 'make':
+    ensure => present
+  } ->
+
+  exec { 'gem install flapjack-diner':
+    path   => "/usr/bin:/usr/sbin:/bin",
+    unless => "gem list --local | grep flapjack-diner"
+  } ->
+
+  exec { 'gem install pry':
+    path   => "/usr/bin:/usr/sbin:/bin",
+    unless => "gem list --local | grep pry"
+  } ->
 
 }
