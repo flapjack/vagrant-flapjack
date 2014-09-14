f942dae0f2144063a69c7661dcb81434193099e4
Lindsay Holmwood <lindsay@holmwood.id.au>
2014-09-14 14:22:50 -0400

    Add httpbroker example

---
+++ tutorial/5-httpbroker-example.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby 
+
+require 'pry'
+require 'httparty'
+require 'json'
+
+# Spawn the httpbroker in the background
+httpbroker = "flapjack receiver httpbroker --interval=1s"
+pid = Process.spawn(httpbroker, [:out,:err] => '/dev/null')
+at_exit do
+  Process.kill(15, pid)
+end
+
+# Wait for the httpbroker to start
+sleep 2 
+
+# Build up a state, submit it to the httpbroker
+state = { :entity => "foo-app-01", :check => "Maple Syrup", :state => "ok", :summary => "Everything is delicious", :ttl => 15 }
+response = HTTParty.post("http://localhost:3090/state", :body => state.to_json)
+
+binding.pry
