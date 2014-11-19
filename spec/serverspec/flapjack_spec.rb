90cb8301c3d959a0e80c8c559f0e745a33ad19ba
Sarah Kowalik <sarah.kowalik@bulletproof.net>
2014-11-19 13:59:58 +1100

    Revert "centos packages should not autostart" Lets test that they're not enabled by default, but that when they're started, they actually work.
    This reverts commit 1eb2bb4de480811ae9f17fd846dec7901942ba52.

--- spec/serverspec/flapjack_spec.rb
+++ spec/serverspec/flapjack_spec.rb
@@ -4,56 +4,29 @@ describe package('flapjack') do
   it { should be_installed }
 end
 
-describe service('flapjack'), :if => os[:family] == 'ubuntu' do
+describe service('flapjack') do
   it { should be_enabled   }
   it { should be_running   }
 end
 
-describe service('flapjack'), :if => os[:family] == 'redhat' do
-  it { should_not be_enabled   }
-  it { should_not be_running   }
-end
-
-describe process("redis-server"), :if => os[:family] == 'ubuntu' do
+describe process("redis-server") do
   it { should be_running }
   its(:args) { should match /0.0.0.0:6380/ }
 end
 
-describe process("redis-server"), :if => os[:family] == 'redhat' do
-  it { should_not be_running }
-end
-
-describe process("flapjack"), :if => os[:family] == 'ubuntu' do
+describe process("flapjack") do
   it { should be_running }
   its(:args) { should match   /\/opt\/flapjack\/bin\/flapjack server start/ }
 end
 
-describe process("flapjack"), :if => os[:family] == 'redhat' do
-  it { should_not be_running }
-end
-
-describe port(3080), :if => os[:family] == 'ubuntu' do
+describe port(3080) do
   it { should be_listening }
 end
-
-describe port(3081), :if => os[:family] == 'ubuntu' do
+describe port(3081) do
   it { should be_listening }
 end
-
-describe port(6380), :if => os[:family] == 'ubuntu' do
+describe port(6380) do
   it { should be_listening }
-end
-
-describe port(3080), :if => os[:family] == 'redhat' do
-  it { should_not be_listening }
-end
-
-describe port(3081), :if => os[:family] == 'redhat' do
-  it { should_not be_listening }
-end
-
-describe port(6380), :if => os[:family] == 'redhat' do
-  it { should_not be_listening }
 end
 
 describe command('/opt/flapjack/bin/flapjack receiver httpbroker --help') do
